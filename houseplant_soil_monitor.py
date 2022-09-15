0a90587e089ba733df4985c842ffeaeefe460980
Danica Fine <dfine@confluent.io>
2022-09-15 15:44:38 -0700

    update polling script

--- houseplant_soil_monitor.py
+++ houseplant_soil_monitor.py
@@ -4,17 +4,14 @@ import logging
 
 from board import SCL, SDA
 import busio
-from confluent_kafka import SerializingProducer
-from confluent_kafka.serialization import StringSerializer
+from confluent_kafka import SerializingProducer, DeserializingConsumer
+from confluent_kafka.serialization import StringSerializer, StringDeserializer
 from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.schema_registry.avro import AvroSerializer, AvroDeserializer
 
 from adafruit_seesaw.seesaw import Seesaw
 
 import avro_helper
-
-TOUCH_HI = 1200
-TOUCH_LO = 600
 
 # set up logging
 logger = logging.getLogger('soil_monitor')
@@ -24,44 +21,78 @@ fh.setLevel(logging.WARN)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger.addHandler(fh)
 
+TOUCH_HI = 1200
+TOUCH_LO = 600
+
+READINGS_TOPIC = 'houseplant-readings'
+MAPPINGS_TOPIC = 'houseplant-sensor-mapping'
+
 # fetches the configs from the available file
 CONFIGS_FILE = './configs/configs.yaml'
 CONFIGS = {}
 with open(CONFIGS_FILE, 'r') as config_file:
     CONFIGS = yaml.load(config_file, Loader=yaml.CLoader)
 
-
 # set up schema registry
-sr_conf = {
+SR_CONF = {
         'url': CONFIGS['schema-registry']['schema.registry.url'],
         'basic.auth.user.info': CONFIGS['schema-registry']['basic.auth.user.info']
 }
-schema_registry_client = SchemaRegistryClient(sr_conf)
+SR_CLIENT = SchemaRegistryClient(SR_CONF)
 
 # set up Kafka producer
-reading_avro_serializer = AvroSerializer(
-        schema_registry_client = schema_registry_client,
+READINGS_SERIALIZER = AvroSerializer(
+        schema_registry_client = SR_CLIENT,
         schema_str = avro_helper.reading_schema,
         to_dict = avro_helper.Reading.reading_to_dict
 )
 
-producer_conf = CONFIGS['kafka']
-producer_conf['value.serializer'] = reading_avro_serializer
-producer = SerializingProducer(producer_conf)
+PRODUCER_CONF = CONFIGS['kafka']
+PRODUCER_CONF['value.serializer'] = READINGS_SERIALIZER
+PRODUCER = SerializingProducer(PRODUCER_CONF)
 
-topic = 'houseplant-readings'
+# set up Kafka Consumer details
+MAPPINGS_DESERIALZIER = AvroDeserializer(
+        schema_registry_client = SR_CLIENT,
+        schema_str = avro_helper.mapping_schema,
+        to_dict = avro_helper.Mapping.mapping_to_dict
+)
 
-# set up mapping between id and address
-plant_addresses = {
-    '0': 0x39,
-    '3': 0x37,
-    '4': 0x36,
-    '5': 0x38
-}
+CONSUMER_CONF = CONFIGS['kafka']
+CONSUMER_CONF['value.deserializer'] = MAPPINGS_DESERIALZIER
+CONSUMER_CONF['group.id'] = 'sensor-mapping-consumer'
+CONSUMER_CONF['auto.offset.reset'] = 'earliest'
+CONSUMER_CONF['enable.auto.commit'] = 'false'
+CONSUMER = DeserializingConsumer(CONSUMER_CONF)
+CONSUMER.subscribe([MAPPINGS_TOPIC])
 
-while True:
+PLANT_ADDRESSES = {}
+
+def consume_sensor_mappings():
+    # loop until there aren't any messages
+    while True:
+        try:
+            msg = CONSUMER.poll(1.0)
+
+            # if no more messages in mapping topic, then move on
+            if msg is None:
+                break
+            else:
+                sensor_id = msg.key()
+                plant_id = msg.value().get('plant_id')
+
+                PLANT_ADDRESSES[sensor_id] = plant_id
+        except SerializerError as e:
+            # Report malformed record, discard results, continue polling
+            logger.error("Message deserialization failed {}".format(e))
+            pass
+        except Exception as e:
+            logger.error("Other exception {}".format(str(e)))
+
+
+def produce_sensor_readings():
     i2c_bus = busio.I2C(SCL, SDA)
-    for k,v in plant_addresses.items():
+    for k,v in PLANT_ADDRESSES.items():
         try:
             ss = Seesaw(i2c_bus, addr=v)
 
@@ -81,12 +112,16 @@ while True:
             ts = int(time.time())
             reading = avro_helper.Reading(int(k), ts, round(touch_percent, 3), round(temp, 3))
 
-            logger.info('Publishing for key ' + str(k))
-            producer.produce(topic, key=k, value=reading) 
-            producer.poll()
+            logger.info("Publishing message: key, value: ({},{})".format(str(k), reading))
+            PRODUCER.produce(READINGS_TOPIC, key=k, value=reading) 
+            PRODUCER.poll()
 
         except Exception as e:
-            print(str(e))
-            logger.error('Got exception ' + str(e))
+            logger.error("Other exception ".format(str(e)))
 
-    time.sleep(5)
+
+while True:
+    consume_sensor_mappings()
+    produce_sensor_readings()
+
+    time.sleep(15)
